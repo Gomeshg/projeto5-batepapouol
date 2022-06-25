8bf5fe4df968c8dd37da370ed41453a68bbe52e4
Hugo Gomes <gomes.hugo00@gmail.com>
2022-06-25 18:11:30 -0300

    Funcionalidade escolher visibilidade: Reservado ou público

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const $menu = document.querySelector('.menu')
 const $users_menu = document.querySelector('.users_menu')
 const $dark_background = document.querySelector('.dark_background')
 
+const $choice_public = document.getElementsByClassName('choice_public')[0]
+const $choice_private = document.getElementsByClassName('choice_private')[0]
+const $item_send_to_all = document.getElementById('item_send_to_all')
+let box_user = [$item_send_to_all]
+let users;
+
+
+
+
+
 // ----------------------------------------------------------------------------------------------------------------- //
 
 
@@ -20,6 +30,11 @@ $div_arrow_icon.addEventListener('click', send_message_click)
 $input_message.addEventListener('keyup', send_message_enter)
 $person_icon.addEventListener('click', menu)
 $dark_background.addEventListener('click', exit_menu)
+// box_user.map( item => item.addEventListener('click', box_user_checked ))
+$choice_private.addEventListener('click', box_visibility_checked)
+$choice_public.addEventListener('click', box_visibility_checked)
+
+
 
 function responsive_layout(){
     let width = window.screen.width;
@@ -67,20 +82,83 @@ function send_message_enter(e){
     }
 }
 
-
 function menu(){
     $menu.style.opacity = '1'
     $menu.style.transform = 'translateX(-0%)';
     $dark_background.style.opacity = '1'
     $dark_background.style.zIndex = '1'
 }
-
 function exit_menu(){
     $dark_background.style.opacity = '0'
     $dark_background.style.zIndex = '-1'
     $menu.style.transform = 'translateX(100%)';
     $menu.style.opacity = '1';
 }
+
+function item_checked(e){
+    
+    let box_item = convert_box_item_in_object(e.currentTarget)
+    let checked = convert_box_item_in_object(box_item.div_checked)
+
+
+    
+
+    if(checked.simbol.style.opacity ==  ''){
+        checked.simbol.style.opacity = '1';
+        box_item.classList.add('selected');
+    }
+    else if(checked.simbol.style.opacity == '0'){
+        checked.simbol.style.opacity = '1';
+        box_item.classList.add('selected');
+    }
+    else if(checked.simbol.style.opacity == '1'){
+        checked.simbol.style.opacity = '0';
+        box_item.classList.remove('selected')
+    }
+}
+
+
+
+function box_visibility_checked(e){
+
+
+    let box_item = e.currentTarget
+    let icon = get_icon_from_box_item(box_item)
+
+    let icon_choice_private = get_icon_from_box_item($choice_private)
+
+    let icon_choice_public = get_icon_from_box_item($choice_public)
+   
+ 
+    if($choice_private.classList.contains('selected') || $choice_public.classList.contains('selected')){
+        console.log('first if')
+
+        if($choice_private.classList.contains('selected')){
+            console.log('second if')
+            
+            $choice_private.classList.remove('selected')
+            icon_choice_private.style.opacity = '0';
+
+            box_item.classList.add('selected')
+            icon.style.opacity = '1'
+        }
+        else if($choice_public.classList.contains('selected')){
+            console.log('else if')
+
+            $choice_public.classList.remove('selected')
+            icon_choice_public.style.opacity = '0';
+
+            box_item.classList.add('selected')
+            icon.style.opacity = '1'
+        }
+    }
+    else{
+        console.log('else')
+        icon.style.opacity = '1'
+        box_item.classList.add('selected')
+    }
+}
+
 
 
 // ----------------------------------------------------------------------------------------------------------------- //
@@ -90,9 +168,14 @@ let user;
 set_user()
 get_messages()
 get_users()
+setTimeout(get_itens_in_box_user, 1000)
+add_checked_event_to_box_visibility()
+// add_event_to_box_item()
 setInterval(keep_user_online, 5000)
 setInterval(get_messages, 3000)
 setInterval(get_users, 10000)
+setInterval(get_itens_in_box_user, 10000)
+// setInterval(add_event_to_itens_menu, 10000)
 setInterval(last_card_scrollIntoView, 3000)
 
 
@@ -189,11 +272,31 @@ function show_users(promise){
     for(let i = 0; i < data.length; i++){
 
         $users_menu.innerHTML += 
-        `<div class="item_menu">
-            <img class="user adjust_icon" src="./imagens/user.png" alt="User icon">
-            <p>${data[i].name}</p>
+        `<div class="box_item">
+            <div class="item">
+                <img class="user_icon" src="./imagens/user.png" alt="User icon">
+                <p>${data[i].name}</p>
+            </div>
+
+            <div class="box_checked_simbol">
+                <img class="checked_simbol" src="./imagens/checked.png" alt="Checked simbol">
+            </div>
         </div>`;
     }
+}
+
+
+function get_itens_in_box_user(){
+
+    let $users_menu = document.getElementsByClassName('users_menu')
+    $users_menu = convert_htmlCollection_in_array($users_menu)[0]
+    
+    users = get_children_from($users_menu)
+
+    box_user = [$item_send_to_all]
+
+    users.map( user => box_user.push(user))
+    console.log(box_user)
 }
 
 
@@ -220,4 +323,52 @@ function update_window(){
 }
 
 
+function get_elements_by_className(Class){
+
+    let item = document.getElementsByClassName(Class)
+    item = convert_htmlCollection_in_array(item)
+
+    return item;
+}
+
+
+function convert_htmlCollection_in_array(html_collection){
+
+    let array = [...html_collection]
+    return array;
+}
+
+// function get_children_from_box_item(box){
+
+//     box = get_children_from(box)
+
+//     let object = {}
+//     if(box.length === 2){
+//         object.item = box[0]
+//         object.div_checked = box[1]
+//     }
+//     else if( box.length === 1){
+//         object.checked = box[0]
+//     }
+
+//     return object;
+// }
+
+function get_children_from(item){
+
+    item = item.children
+    item = convert_htmlCollection_in_array(item)
+    return item;
+}
+
+function get_icon_from_box_item(box){
+
+    box = get_children_from(box)
+
+    let div_checked = box[1]
+
+    let icon_checked = get_children_from(div_checked)
+
+    return icon_checked[0];
+}
 // ----------------------------------------------------------------------------------------------------------------- //
